63a223999a20c6c5ba8c2a634d8119abc9d5677e
afshin.nj@ <afshin.nj@gmail.com>
2015-02-03 19:38:47 +0330

    Added Persian language

---
+++ messages/fa-IR/user.php
@@ -0,0 +1,107 @@
+<?php
+
+return [
+
+    // models
+    'ID' => 'شناسه',
+    'User ID' => 'شناسه کاربر',
+    'Create Time' => 'زمان ایجاد',
+    'Update Time' => 'زمان بروزرسانی',
+    'Full Name' => 'نام کامل',
+	//'Mobile' => 'موبایل',
+	//'Avatar' => 'آواتار',
+		
+    'Name' => 'نام',
+    'Can Admin' => 'سرپرست',
+
+    'Role' => 'نقش',
+    'Role ID' => 'شنانه نقش',
+    'Status' => 'وضعیت',
+    'Email' => 'ایمیل',
+    'New Email' => 'ایمیل جدید',
+    'Username' => 'نام کاربری',
+    'Password' => 'رمز عبور',
+    'Auth Key' => 'کلید تایید',
+    'Api Key' => 'کلید API',
+    'Login Ip' => 'IP ورود',
+    'Login Time' => 'زمان ورود',
+    'Create Ip' => 'ایجاد Ip',
+    'Ban Time' => 'زمان مستود',
+    'Ban Reason' => 'دلیل مستود شدن',
+    'Current Password' => 'رمز عبور فعلی',
+    'New Password' => 'رمز عبور جدید',
+//    'New Password Confirm' => '',
+    'Email Confirmation' => 'ایمیل تایید',
+
+    'Provider' => 'ارائه دهنده',
+    'Provider ID' => 'شناسه ارائه دهنده',
+    'Provider Attributes' => 'ویژگی های ارائه دهنده',
+
+    'Type' => 'نوع',
+    'Key' => 'کلید',
+    'Consume Time' => 'زمان مصرف',
+    'Expire Time' => 'زمان انقضاء ',
+
+    // models/forms
+    'Email not found' => 'ایمیل یافت نشد',
+	'Email / Username' => 'ایمیل / نام کاربری',
+    'Email / username not found' => 'ایمیل/نام کاربری یافت نشد',
+    'Username not found' => 'نام کاربری یافت نشد',
+    'User is banned - {banReason}' => 'کاربر ممنوع است - {banReason}',
+    'Incorrect password' => 'رمز ورود اشتباه است',
+    'Remember Me' => 'من را به یاد داشته باش',
+    'Email is already active' => 'پست الکترونیک در حال حاضر فعال است',
+//    'Passwords do not match' => '',
+//    '{attribute} can contain only letters, numbers, and "_"' => '',
+
+    // controllers
+    'Successfully registered [ {displayName} ]' => 'با موفقیت ثبت شد [ {displayName} ]',
+    ' - Please check your email to confirm your account' => 'لطفا ایمیل خود را برای تایید حساب خود را چک کنید -',
+    'Account updated' => 'حساب به روز شده',
+    'Profile updated' => 'مشخصات به روز شده',
+    'Confirmation email resent' => 'تایید مجددا ایمیل',
+    'Email change cancelled' => 'تغییر ایمیل لغو شد',
+    'Instructions to reset your password have been sent' => 'دستورالعمل رمز عبور خود را برای تنظیم مجدد ارسال شده است',
+
+    // mail
+    'Please confirm your email address by clicking the link below:' => 'لطفا آدرس ایمیل خود را تأیید کنید با کلیک روی لینک زیر:',
+    'Please use this link to reset your password:' => 'لطفا از این لینک استفاده نمایید:',
+
+    // admin views
+    'Users' => 'کاربرها',
+    'Banned' => 'ممنوع',
+    'Create' => 'ایجاد',
+    'Update' => 'بروز رسانی',
+    'Delete' => 'حذف',
+    'Search' => 'جستجو',
+    'Reset' => 'بازسازی',
+    'Create {modelClass}' => 'ایحاد {modelClass}',
+    'Update {modelClass}: ' => 'بروزرسانی {modelClass}: ',
+    'Are you sure you want to delete this item?' => 'آیا شما مطمئن هستید که میخواهید این آیتم را حذف کنید؟',
+
+    // default views
+    'Account' => 'حساب کاربری',
+    'Pending email confirmation: [ {newEmail} ]' => 'در انتظار تایید ایمیل: [ {newEmail} ]',
+    'Cancel' => 'انصراف',
+    'Changing your email requires email confirmation' => 'تغییر ایمیل شما نیاز به ایمیل',
+    'Confirmed' => 'تایید شده',
+    'Error' => 'خطا',
+    'Your email [ {email} ] has been confirmed' => 'ایمیل شما [ {email} ] تایید شده است',
+    'Go to my account' => 'برو به صفحه حساب من',
+    'Go home' => 'برو به خانه',
+    'Log in here' => 'ورود به سیستم',
+    'Invalid key' => 'کلید نامعتبر است',
+    'Forgot password' => 'فراموشی رمز عبور',
+    'Submit' => 'ارسال',
+    'Yii 2 User' => 'Usuario de Yii 2',
+    'Login' => 'ورود',
+    'Register' => 'ثبت نام',
+    'Logout' => 'خروج', // i dont think this is used ...
+    'Resend confirmation email' => 'ارسال مجدد ایمیل تاییدیه',
+    'Profile' => 'نمایه',
+    'Please fill out the following fields to login:' => 'لطفا از فرم زیر برای ورود به سایت استفاده کنید:',
+    'Please fill out the following fields to register:' => 'لطفا از فرم زیر برای ثبت نام استفاده کنید:',
+    'Resend' => 'ارسال دوباره',
+    'Password has been reset' => 'رمز عبور بازنشانی شده است',
+	'Current password incorrect' => 'رمز عبور فعلی اشتباه است.',
+];
